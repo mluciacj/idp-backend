4a50fa732c7aed5e342118e6079bc76b51376a76
mluciacjds <mluciacjds@gmail.com>
2025-08-07 22:22:43 -0300

    fix: cors regex dev

--- app/main.py
+++ app/main.py
@@ -1,14 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1.endpoints.upload import router as upload_router
-from app.api.v1.endpoints.documents import router as documents_router
-from app.api.v1.endpoints.authorizations import router as authorizations_router
-from app.api.v1.endpoints.session import router as session_router
-from app.api.v1.endpoints.subscriptions import router as subscriptions_router
-from app.api.v1.endpoints.billing import router as billing_router
-from app.api.v1.endpoints.pageviews import router as pageviews_router
-from app.api.v1.endpoints.funnel import router as funnel_router
-from app.api.v1.endpoints.userevents import router as userevents_router
+
 
 app = FastAPI()
 app.add_middleware(
@@ -19,11 +11,21 @@ app.add_middleware(
         "http://localhost:5000",
         "http://127.0.0.1:5000",
     ],
-    allow_origin_regex=r"^https:\/\/.*\.replit\.app$",
+    allow_origin_regex=r"^https:\/\/.*\.replit\.dev$",
     allow_credentials=True,                 # you need this for Authorization/cookies
     allow_methods=["GET","POST","PUT","DELETE","OPTIONS","PATCH"],
     allow_headers=["*"],                    # includes Authorization
 )
+
+from app.api.v1.endpoints.upload import router as upload_router
+from app.api.v1.endpoints.documents import router as documents_router
+from app.api.v1.endpoints.authorizations import router as authorizations_router
+from app.api.v1.endpoints.session import router as session_router
+from app.api.v1.endpoints.subscriptions import router as subscriptions_router
+from app.api.v1.endpoints.billing import router as billing_router
+from app.api.v1.endpoints.pageviews import router as pageviews_router
+from app.api.v1.endpoints.funnel import router as funnel_router
+from app.api.v1.endpoints.userevents import router as userevents_router
 
 app.include_router(upload_router)
 app.include_router(documents_router)
